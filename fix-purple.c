3c8ccb32774306273117e0fa80e7e50171cbd0cc
Felipe Contreras <felipe.contreras@gmail.com>
2007-12-16 00:02:44 -0600

    Code formatting.

--- fix-purple.c
+++ fix-purple.c
@@ -20,8 +20,8 @@
 
 void
 fix_purple_buddy_set_alias (PurpleConnection *gc,
-			    const char *who,
-			    const char *alias)
+                            const char *who,
+                            const char *alias)
 {
     PurpleAccount *account = purple_connection_get_account (gc);
     GSList *buddies = purple_find_buddies (account, who);
@@ -29,23 +29,23 @@ fix_purple_buddy_set_alias (PurpleConnection *gc,
 
     while (buddies != NULL)
     {
-	b = buddies->data;
-	buddies = g_slist_delete_link (buddies, buddies);
+        b = buddies->data;
+        buddies = g_slist_delete_link (buddies, buddies);
 
-	if ((b->alias == NULL && alias == NULL) ||
-	    (b->alias && alias && !strcmp (b->alias, alias)))
-	{
-	    continue;
-	}
+        if ((b->alias == NULL && alias == NULL) ||
+            (b->alias && alias && !strcmp (b->alias, alias)))
+        {
+            continue;
+        }
 
-	purple_blist_alias_buddy (b, alias);
+        purple_blist_alias_buddy (b, alias);
     }
 }
 
 void
 fix_purple_buddy_set_friendly (PurpleConnection *gc,
-			       const char *who,
-			       const char *friendly)
+                               const char *who,
+                               const char *friendly)
 {
     PurpleAccount *account = purple_connection_get_account (gc);
     GSList *buddies = purple_find_buddies (account, who);
@@ -53,15 +53,15 @@ fix_purple_buddy_set_friendly (PurpleConnection *gc,
 
     while (buddies != NULL)
     {
-	b = buddies->data;
-	buddies = g_slist_delete_link (buddies, buddies);
+        b = buddies->data;
+        buddies = g_slist_delete_link (buddies, buddies);
 
-	if ((b->server_alias == NULL && friendly == NULL) ||
-	    (b->server_alias && friendly && !strcmp (b->server_alias, friendly)))
-	{
-	    continue;
-	}
+        if ((b->server_alias == NULL && friendly == NULL) ||
+            (b->server_alias && friendly && !strcmp (b->server_alias, friendly)))
+        {
+            continue;
+        }
 
-	purple_blist_server_alias_buddy (b, friendly);
+        purple_blist_server_alias_buddy (b, friendly);
     }
 }
