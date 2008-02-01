1e15a226b4291bfe970a7e45953e439d5d79deb8
Felipe Contreras <felipe.contreras@gmail.com>
2008-02-01 02:07:12 +0200

    Cleanup user.c.

--- ab/user.c
+++ ab/user.c
@@ -34,393 +34,406 @@
 #include <account.h>
 
 MsnUser *
-msn_user_new(MsnUserList *userlist, const char *passport)
+msn_user_new (MsnUserList *userlist,
+              const gchar *passport)
 {
-	MsnUser *user;
+    MsnUser *user;
 
-	user = g_new0(MsnUser, 1);
+    user = g_new0 (MsnUser, 1);
 
-	user->userlist = userlist;
+    user->userlist = userlist;
 
-	msn_user_set_passport(user, passport);
+    msn_user_set_passport (user, passport);
 
-	return user;
+    return user;
 }
 
 void
-msn_user_destroy(MsnUser *user)
+msn_user_destroy (MsnUser *user)
 {
-	g_return_if_fail(user != NULL);
+    g_return_if_fail (user != NULL);
 
-	if (user->clientcaps != NULL)
-		g_hash_table_destroy(user->clientcaps);
+    if (user->clientcaps != NULL)
+        g_hash_table_destroy (user->clientcaps);
 
-	if (user->group_ids != NULL)
-		g_list_free(user->group_ids);
+    if (user->group_ids != NULL)
+        g_list_free (user->group_ids);
 
-	if (user->msnobj != NULL)
-		msn_object_destroy(user->msnobj);
+    if (user->msnobj != NULL)
+        msn_object_destroy (user->msnobj);
 
-	g_free(user->passport);
-	g_free(user->friendly_name);
-	g_free(user->store_name);
-	g_free(user->phone.home);
-	g_free(user->phone.work);
-	g_free(user->phone.mobile);
+    g_free (user->passport);
+    g_free (user->friendly_name);
+    g_free (user->store_name);
+    g_free (user->phone.home);
+    g_free (user->phone.work);
+    g_free (user->phone.mobile);
 
-	g_free(user);
+    g_free (user);
 }
 
 void
-msn_user_update(MsnUser *user)
+msn_user_update (MsnUser *user)
 {
-	PurpleAccount *account;
+    PurpleAccount *account;
 
-	account = msn_session_get_account (user->userlist->session);
+    account = msn_session_get_account (user->userlist->session);
 
-	if (user->status != NULL) {
-		if (!strcmp(user->status, "offline") && user->mobile) {
-			purple_prpl_got_user_status(account, user->passport, "offline", NULL);
-			purple_prpl_got_user_status(account, user->passport, "mobile", NULL);
-		} else {
-			purple_prpl_got_user_status(account, user->passport, user->status, NULL);
-			purple_prpl_got_user_status_deactive(account, user->passport, "mobile");
-		}
-	}
+    if (user->status != NULL)
+    {
+        if (!strcmp(user->status, "offline") && user->mobile)
+        {
+            purple_prpl_got_user_status (account, user->passport, "offline", NULL);
+            purple_prpl_got_user_status (account, user->passport, "mobile", NULL);
+        }
+        else
+        {
+            purple_prpl_got_user_status (account, user->passport, user->status, NULL);
+            purple_prpl_got_user_status_deactive (account, user->passport, "mobile");
+        }
+    }
 
-	if (user->idle)
-		purple_prpl_got_user_idle(account, user->passport, TRUE, -1);
-	else
-		purple_prpl_got_user_idle(account, user->passport, FALSE, 0);
+    if (user->idle)
+        purple_prpl_got_user_idle (account, user->passport, TRUE, -1);
+    else
+        purple_prpl_got_user_idle (account, user->passport, FALSE, 0);
 }
 
 void
-msn_user_set_state(MsnUser *user, const char *state)
+msn_user_set_state (MsnUser *user,
+                   const gchar *state)
 {
-	const char *status;
+    const gchar *status;
 
-	if (!g_ascii_strcasecmp(state, "BSY"))
-		status = "busy";
-	else if (!g_ascii_strcasecmp(state, "BRB"))
-		status = "brb";
-	else if (!g_ascii_strcasecmp(state, "AWY"))
-		status = "away";
-	else if (!g_ascii_strcasecmp(state, "PHN"))
-		status = "phone";
-	else if (!g_ascii_strcasecmp(state, "LUN"))
-		status = "lunch";
-	else
-		status = "available";
+    if (!g_ascii_strcasecmp (state, "BSY"))
+        status = "busy";
+    else if (!g_ascii_strcasecmp (state, "BRB"))
+        status = "brb";
+    else if (!g_ascii_strcasecmp (state, "AWY"))
+        status = "away";
+    else if (!g_ascii_strcasecmp (state, "PHN"))
+        status = "phone";
+    else if (!g_ascii_strcasecmp (state, "LUN"))
+        status = "lunch";
+    else
+        status = "available";
 
-	if (!g_ascii_strcasecmp(state, "IDL"))
-		user->idle = TRUE;
-	else
-		user->idle = FALSE;
+    if (!g_ascii_strcasecmp (state, "IDL"))
+        user->idle = TRUE;
+    else
+        user->idle = FALSE;
 
-	user->status = status;
+    user->status = status;
 }
 
 void
-msn_user_set_passport(MsnUser *user, const char *passport)
+msn_user_set_passport (MsnUser *user,
+                       const gchar *passport)
 {
-	g_return_if_fail(user != NULL);
+    g_return_if_fail (user != NULL);
 
-	g_free(user->passport);
-	user->passport = g_strdup(passport);
+    g_free (user->passport);
+    user->passport = g_strdup (passport);
 }
 
 void
-msn_user_set_friendly_name(MsnUser *user, const char *name)
+msn_user_set_friendly_name (MsnUser *user,
+                            const gchar *name)
 {
-	g_return_if_fail(user != NULL);
+    g_return_if_fail (user != NULL);
 
-	g_free(user->friendly_name);
-	user->friendly_name = g_strdup(name);
+    g_free( user->friendly_name);
+    user->friendly_name = g_strdup (name);
 
-	{
-		PurpleAccount *account;
-		PurpleConnection *gc;
-		account = msn_session_get_account (user->userlist->session);
-		gc = purple_account_get_connection(account);
-		fix_purple_buddy_set_friendly(gc, user->passport, user->friendly_name);
-	}
+    {
+        PurpleAccount *account;
+        PurpleConnection *gc;
+        account = msn_session_get_account (user->userlist->session);
+        gc = purple_account_get_connection (account);
+        fix_purple_buddy_set_friendly (gc, user->passport, user->friendly_name);
+    }
 }
 
 void
-msn_user_set_store_name(MsnUser *user, const char *name)
+msn_user_set_store_name (MsnUser *user,
+                         const gchar *name)
 {
-	g_return_if_fail(user != NULL);
+    g_return_if_fail (user != NULL);
 
-	g_free(user->store_name);
-	user->store_name = g_strdup(name);
+    g_free (user->store_name);
+    user->store_name = g_strdup (name);
 
-	{
-		PurpleAccount *account;
-		PurpleConnection *gc;
-		account = msn_session_get_account (user->userlist->session);
-		gc = purple_account_get_connection(account);
-		fix_purple_buddy_set_alias(gc, user->passport, user->store_name);
-	}
+    {
+        PurpleAccount *account;
+        PurpleConnection *gc;
+        account = msn_session_get_account (user->userlist->session);
+        gc = purple_account_get_connection (account);
+        fix_purple_buddy_set_alias (gc, user->passport, user->store_name);
+    }
 }
 
 void
-msn_user_set_buddy_icon(MsnUser *user, PurpleStoredImage *img)
+msn_user_set_buddy_icon (MsnUser *user,
+                         PurpleStoredImage *img)
 {
-	MsnObject *msnobj = msn_user_get_object(user);
+    MsnObject *msnobj = msn_user_get_object (user);
 
-	g_return_if_fail(user != NULL);
+    g_return_if_fail (user);
 
-	if (img == NULL)
-		msn_user_set_object(user, NULL);
-	else
-	{
-		PurpleCipherContext *ctx;
-		char *buf;
-		gconstpointer data = purple_imgstore_get_data(img);
-		size_t size = purple_imgstore_get_size(img);
-		char *base64;
-		unsigned char digest[20];
+    if (!img)
+        msn_user_set_object (user, NULL);
+    else
+    {
+        PurpleCipherContext *ctx;
+        char *buf;
+        gconstpointer data = purple_imgstore_get_data (img);
+        size_t size = purple_imgstore_get_size (img);
+        char *base64;
+        unsigned char digest[20];
 
-		if (msnobj == NULL)
-		{
-			msnobj = msn_object_new();
-			msn_object_set_local(msnobj);
-			msn_object_set_type(msnobj, MSN_OBJECT_USERTILE);
-			msn_object_set_location(msnobj, "TFR2C2.tmp");
-			msn_object_set_creator(msnobj, msn_user_get_passport(user));
+        if (msnobj == NULL)
+        {
+            msnobj = msn_object_new ();
+            msn_object_set_local (msnobj);
+            msn_object_set_type (msnobj, MSN_OBJECT_USERTILE);
+            msn_object_set_location (msnobj, "TFR2C2.tmp");
+            msn_object_set_creator (msnobj, msn_user_get_passport (user));
 
-			msn_user_set_object(user, msnobj);
-		}
+            msn_user_set_object (user, msnobj);
+        }
 
-		msn_object_set_image(msnobj, img);
+        msn_object_set_image (msnobj, img);
 
-		/* Compute the SHA1D field. */
-		memset(digest, 0, sizeof(digest));
+        /* Compute the SHA1D field. */
+        memset (digest, 0, sizeof (digest));
 
-		ctx = purple_cipher_context_new_by_name("sha1", NULL);
-		purple_cipher_context_append(ctx, data, size);
-		purple_cipher_context_digest(ctx, sizeof(digest), digest, NULL);
+        ctx = purple_cipher_context_new_by_name ("sha1", NULL);
+        purple_cipher_context_append (ctx, data, size);
+        purple_cipher_context_digest (ctx, sizeof (digest), digest, NULL);
 
-		base64 = purple_base64_encode(digest, sizeof(digest));
-		msn_object_set_sha1d(msnobj, base64);
-		g_free(base64);
+        base64 = purple_base64_encode (digest, sizeof (digest));
+        msn_object_set_sha1d (msnobj, base64);
+        g_free (base64);
 
-		msn_object_set_size(msnobj, size);
+        msn_object_set_size (msnobj, size);
 
-		/* Compute the SHA1C field. */
-		buf = g_strdup_printf(
-			"Creator%sSize%dType%dLocation%sFriendly%sSHA1D%s",
-			msn_object_get_creator(msnobj),
-			msn_object_get_size(msnobj),
-			msn_object_get_type(msnobj),
-			msn_object_get_location(msnobj),
-			msn_object_get_friendly(msnobj),
-			msn_object_get_sha1d(msnobj));
+        /* Compute the SHA1C field. */
+        buf = g_strdup_printf ("Creator%sSize%dType%dLocation%sFriendly%sSHA1D%s",
+                               msn_object_get_creator (msnobj),
+                               msn_object_get_size (msnobj),
+                               msn_object_get_type (msnobj),
+                               msn_object_get_location (msnobj),
+                               msn_object_get_friendly (msnobj),
+                               msn_object_get_sha1d (msnobj));
 
-		memset(digest, 0, sizeof(digest));
+        memset (digest, 0, sizeof (digest));
 
-		purple_cipher_context_reset(ctx, NULL);
-		purple_cipher_context_append(ctx, (const guchar *)buf, strlen(buf));
-		purple_cipher_context_digest(ctx, sizeof(digest), digest, NULL);
-		purple_cipher_context_destroy(ctx);
-		g_free(buf);
+        purple_cipher_context_reset (ctx, NULL);
+        purple_cipher_context_append (ctx, (const guchar *)buf, strlen(buf));
+        purple_cipher_context_digest (ctx, sizeof (digest), digest, NULL);
+        purple_cipher_context_destroy (ctx);
+        g_free (buf);
 
-		base64 = purple_base64_encode(digest, sizeof(digest));
-		msn_object_set_sha1c(msnobj, base64);
-		g_free(base64);
-	}
+        base64 = purple_base64_encode (digest, sizeof (digest));
+        msn_object_set_sha1c( msnobj, base64);
+        g_free (base64);
+    }
 }
 
 void
-msn_user_add_group_id(MsnUser *user, int id)
+msn_user_add_group_id (MsnUser *user,
+                       gint id)
 {
-	MsnUserList *userlist;
-	PurpleAccount *account;
-	PurpleBuddy *b;
-	PurpleGroup *g;
-	const char *passport;
-	const char *group_name;
+    MsnUserList *userlist;
+    PurpleAccount *account;
+    PurpleBuddy *b;
+    PurpleGroup *g;
+    const gchar *passport;
+    const gchar *group_name;
 
-	g_return_if_fail(user != NULL);
-	g_return_if_fail(id >= 0);
+    g_return_if_fail (user);
+    g_return_if_fail (id >= 0);
 
-	user->group_ids = g_list_append(user->group_ids, GINT_TO_POINTER(id));
+    user->group_ids = g_list_append (user->group_ids, GINT_TO_POINTER (id));
 
-	userlist = user->userlist;
-	account = msn_session_get_account (userlist->session);
-	passport = msn_user_get_passport(user);
+    userlist = user->userlist;
+    account = msn_session_get_account (userlist->session);
+    passport = msn_user_get_passport (user);
 
-	group_name = msn_userlist_find_group_name(userlist, id);
+    group_name = msn_userlist_find_group_name (userlist, id);
 
-	g = purple_find_group(group_name);
+    g = purple_find_group( group_name);
 
-	/* If the group is not there, add it */
-	if ((id == 0) && (g == NULL))
-	{
-		g = purple_group_new(group_name);
-		purple_blist_add_group(g, NULL);
-	}
+    /* If the group is not there, add it */
+    if ((id == 0) && (g == NULL))
+    {
+        g = purple_group_new (group_name);
+        purple_blist_add_group (g, NULL);
+    }
 
-	b = purple_find_buddy_in_group(account, passport, g);
+    b = purple_find_buddy_in_group (account, passport, g);
 
-	if (b == NULL)
-	{
-		b = purple_buddy_new(account, passport, NULL);
-		purple_blist_add_buddy(b, NULL, g, NULL);
-	}
+    if (!b)
+    {
+        b = purple_buddy_new (account, passport, NULL);
+        purple_blist_add_buddy (b, NULL, g, NULL);
+    }
 
-	b->proto_data = user;
+    b->proto_data = user;
 
-	/* If we have users on the 'Individuals' group, remove them. */
-	if (id != 0)
-	{
-		group_name = msn_userlist_find_group_name(userlist, 0);
-		g = purple_find_group(group_name);
-		b = purple_find_buddy_in_group(account, passport, g);
+    /* If we have users on the 'Individuals' group, remove them. */
+    if (id != 0)
+    {
+        group_name = msn_userlist_find_group_name (userlist, 0);
+        g = purple_find_group (group_name);
+        b = purple_find_buddy_in_group (account, passport, g);
 
-		if (b)
-		{
-			purple_blist_remove_buddy(b);
-		}
-	}
+        if (b)
+        {
+            purple_blist_remove_buddy (b);
+        }
+    }
 }
 
 void
-msn_user_remove_group_id(MsnUser *user, int id)
+msn_user_remove_group_id (MsnUser *user,
+                          gint id)
 {
-	g_return_if_fail(user != NULL);
-	g_return_if_fail(id >= 0);
+    g_return_if_fail (user);
+    g_return_if_fail (id >= 0);
 
-	user->group_ids = g_list_remove(user->group_ids, GINT_TO_POINTER(id));
+    user->group_ids = g_list_remove (user->group_ids, GINT_TO_POINTER (id));
 }
 
 void
-msn_user_set_home_phone(MsnUser *user, const char *number)
+msn_user_set_home_phone (MsnUser *user,
+                         const gchar *number)
 {
-	g_return_if_fail(user != NULL);
+    g_return_if_fail (user);
 
-	if (user->phone.home != NULL)
-		g_free(user->phone.home);
+    g_free (user->phone.home);
 
-	user->phone.home = (number == NULL ? NULL : g_strdup(number));
+    user->phone.home = (number == NULL ? NULL : g_strdup(number));
 }
 
 void
-msn_user_set_work_phone(MsnUser *user, const char *number)
+msn_user_set_work_phone (MsnUser *user,
+                         const gchar *number)
 {
-	g_return_if_fail(user != NULL);
+    g_return_if_fail (user);
 
-	if (user->phone.work != NULL)
-		g_free(user->phone.work);
+    g_free (user->phone.work);
 
-	user->phone.work = (number == NULL ? NULL : g_strdup(number));
+    user->phone.work = (number == NULL ? NULL : g_strdup(number));
 }
 
 void
-msn_user_set_mobile_phone(MsnUser *user, const char *number)
+msn_user_set_mobile_phone (MsnUser *user,
+                           const gchar *number)
 {
-	g_return_if_fail(user != NULL);
+    g_return_if_fail (user);
 
-	if (user->phone.mobile != NULL)
-		g_free(user->phone.mobile);
+    g_free (user->phone.mobile);
 
-	user->phone.mobile = (number == NULL ? NULL : g_strdup(number));
+    user->phone.mobile = (number == NULL ? NULL : g_strdup(number));
 }
 
 void
-msn_user_set_object(MsnUser *user, MsnObject *obj)
+msn_user_set_object (MsnUser *user,
+                     MsnObject *obj)
 {
-	g_return_if_fail(user != NULL);
+    g_return_if_fail (user);
 
-	if (user->msnobj != NULL)
-		msn_object_destroy(user->msnobj);
+    if (user->msnobj)
+        msn_object_destroy (user->msnobj);
 
-	user->msnobj = obj;
+    user->msnobj = obj;
 
-	if (user->list_op & MSN_LIST_FL_OP)
-		msn_queue_buddy_icon_request(user);
+    if (user->list_op & MSN_LIST_FL_OP)
+        msn_queue_buddy_icon_request (user);
 }
 
 void
-msn_user_set_client_caps(MsnUser *user, GHashTable *info)
+msn_user_set_client_caps (MsnUser *user,
+                          GHashTable *info)
 {
-	g_return_if_fail(user != NULL);
-	g_return_if_fail(info != NULL);
+    g_return_if_fail (user);
+    g_return_if_fail (info);
 
-	if (user->clientcaps != NULL)
-		g_hash_table_destroy(user->clientcaps);
+    if (user->clientcaps)
+        g_hash_table_destroy (user->clientcaps);
 
-	user->clientcaps = info;
+    user->clientcaps = info;
 }
 
-const char *
-msn_user_get_passport(const MsnUser *user)
+const gchar *
+msn_user_get_passport (const MsnUser *user)
 {
-	g_return_val_if_fail(user != NULL, NULL);
+    g_return_val_if_fail (user, NULL);
 
-	return user->passport;
+    return user->passport;
 }
 
-const char *
-msn_user_get_friendly_name(const MsnUser *user)
+const gchar *
+msn_user_get_friendly_name (const MsnUser *user)
 {
-	g_return_val_if_fail(user != NULL, NULL);
+    g_return_val_if_fail (user, NULL);
 
-	return user->friendly_name;
+    return user->friendly_name;
 }
 
-const char *
-msn_user_get_store_name(const MsnUser *user)
+const gchar *
+msn_user_get_store_name (const MsnUser *user)
 {
-	g_return_val_if_fail(user != NULL, NULL);
+    g_return_val_if_fail (user, NULL);
 
-	return user->store_name;
+    return user->store_name;
 }
 
-const char *
+const gchar *
 msn_user_get_home_phone(const MsnUser *user)
 {
-	g_return_val_if_fail(user != NULL, NULL);
+    g_return_val_if_fail (user, NULL);
 
-	return user->phone.home;
+    return user->phone.home;
 }
 
-const char *
-msn_user_get_work_phone(const MsnUser *user)
+const gchar *
+msn_user_get_work_phone (const MsnUser *user)
 {
-	g_return_val_if_fail(user != NULL, NULL);
+    g_return_val_if_fail (user, NULL);
 
-	return user->phone.work;
+    return user->phone.work;
 }
 
-const char *
-msn_user_get_mobile_phone(const MsnUser *user)
+const gchar *
+msn_user_get_mobile_phone (const MsnUser *user)
 {
-	g_return_val_if_fail(user != NULL, NULL);
+    g_return_val_if_fail( user, NULL);
 
-	return user->phone.mobile;
+    return user->phone.mobile;
 }
 
 MsnObject *
-msn_user_get_object(const MsnUser *user)
+msn_user_get_object (const MsnUser *user)
 {
-	g_return_val_if_fail(user != NULL, NULL);
+    g_return_val_if_fail (user, NULL);
 
-	return user->msnobj;
+    return user->msnobj;
 }
 
 GHashTable *
-msn_user_get_client_caps(const MsnUser *user)
+msn_user_get_client_caps (const MsnUser *user)
 {
-	g_return_val_if_fail(user != NULL, NULL);
+    g_return_val_if_fail(user, NULL);
 
-	return user->clientcaps;
+    return user->clientcaps;
 }
 
 GList *
-msn_user_get_group_ids(const MsnUser *user)
+msn_user_get_group_ids (const MsnUser *user)
 {
-	g_return_val_if_fail(user != NULL, NULL);
+    g_return_val_if_fail (user, NULL);
 
-	return user->group_ids;
+    return user->group_ids;
 }
