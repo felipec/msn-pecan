a2565fa97dc1af99dc5e4d4ef55ac1b24235d1c6
Felipe Contreras <felipe.contreras@gmail.com>
2008-07-13 23:54:35 +0300

    Add oim request processing.

--- pecan_oim.c
+++ pecan_oim.c
@@ -1,6 +1,8 @@
 #include "pecan_oim_private.h"
 #include "io/pecan_ssl_conn.h"
 
+#include "pecan_log.h"
+
 typedef struct OimRequest OimRequest;
 
 struct OimRequest
@@ -60,11 +62,32 @@ pecan_oim_session_free (PecanOimSession *oim_session)
     g_free (oim_session);
 }
 
+static inline void
+oim_process_requests (PecanOimSession *oim_session)
+{
+    OimRequest *oim_request;
+
+    oim_request = g_queue_pop_head (oim_session->request_queue);
+
+    if (!oim_request)
+        return;
+
+    pecan_debug ("%s: %s", oim_request->passport, oim_request->message_id);
+    oim_process_requests (oim_session);
+}
+
 void
 pecan_oim_session_request (PecanOimSession *oim_session,
                            const gchar *passport,
                            const gchar *message_id)
 {
+    gboolean initial;
+
+    initial = g_queue_is_empty (oim_session->request_queue);
+
     g_queue_push_tail (oim_session->request_queue,
                        oim_request_new (passport, message_id));
+
+    if (initial)
+        oim_process_requests (oim_session);
 }
