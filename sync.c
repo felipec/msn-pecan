2a8c18589afa5ef926b223d2267bbb03f0b8019c
Felipe Contreras <felipe.contreras@gmail.com>
2008-12-13 19:29:25 +0200

    sync: Cleanup.

--- sync.c
+++ sync.c
@@ -42,274 +42,279 @@
 static MsnTable *cbs_table;
 
 static void
-blp_cmd(MsnCmdProc *cmdproc, MsnCommand *cmd)
+blp_cmd (MsnCmdProc *cmdproc,
+         MsnCommand *cmd)
 {
-	PurpleConnection *gc = cmdproc->session->account->gc;
-	const char *list_name;
+    PurpleConnection *gc = cmdproc->session->account->gc;
+    const char *list_name;
 
-	list_name = cmd->params[0];
+    list_name = cmd->params[0];
 
-	if (!g_ascii_strcasecmp(list_name, "AL"))
-	{
-		/*
-		 * If the current setting is AL, messages from users who
-		 * are not in BL will be delivered.
-		 *
-		 * In other words, deny some.
-		 */
-		gc->account->perm_deny = PURPLE_PRIVACY_DENY_USERS;
-	}
-	else
-	{
-		/* If the current setting is BL, only messages from people
-		 * who are in the AL will be delivered.
-		 *
-		 * In other words, permit some.
-		 */
-		gc->account->perm_deny = PURPLE_PRIVACY_ALLOW_USERS;
-	}
+    if (!g_ascii_strcasecmp (list_name, "AL"))
+    {
+        /*
+         * If the current setting is AL, messages from users who
+         * are not in BL will be delivered.
+         *
+         * In other words, deny some.
+         */
+        gc->account->perm_deny = PURPLE_PRIVACY_DENY_USERS;
+    }
+    else
+    {
+        /* If the current setting is BL, only messages from people
+         * who are in the AL will be delivered.
+         *
+         * In other words, permit some.
+         */
+        gc->account->perm_deny = PURPLE_PRIVACY_ALLOW_USERS;
+    }
 }
 
 static void
-prp_cmd(MsnCmdProc *cmdproc, MsnCommand *cmd)
+prp_cmd (MsnCmdProc *cmdproc,
+         MsnCommand *cmd)
 {
-	MsnSession *session = cmdproc->session;
-	PurpleConnection *gc = session->account->gc;
-	const gchar *type, *value;
-	PecanContact *user;
+    MsnSession *session = cmdproc->session;
+    PurpleConnection *gc = session->account->gc;
+    const gchar *type, *value;
+    PecanContact *user;
 
-	type  = cmd->params[0];
-	value = cmd->params[1];
-	user = msn_session_get_contact (session);
+    type  = cmd->params[0];
+    value = cmd->params[1];
+    user = msn_session_get_contact (session);
 
-	if (cmd->param_count == 2)
-	{
-		gchar *tmp;
-		tmp = pecan_url_decode (value);
-		if (!strcmp(type, "PHH"))
-			pecan_contact_set_home_phone(user, tmp);
-		else if (!strcmp(type, "PHW"))
-			pecan_contact_set_work_phone(user, tmp);
-		else if (!strcmp(type, "PHM"))
-			pecan_contact_set_mobile_phone(user, tmp);
-		else if (!strcmp(type, "MFN"))
-			purple_connection_set_display_name(gc, tmp);
-		g_free (tmp);
-	}
-	else
-	{
-		if (!strcmp(type, "PHH"))
-			pecan_contact_set_home_phone(user, NULL);
-		else if (!strcmp(type, "PHW"))
-			pecan_contact_set_work_phone(user, NULL);
-		else if (!strcmp(type, "PHM"))
-			pecan_contact_set_mobile_phone(user, NULL);
-	}
+    if (cmd->param_count == 2)
+    {
+        gchar *tmp;
+        tmp = pecan_url_decode (value);
+        if (strcmp (type, "PHH") == 0)
+            pecan_contact_set_home_phone (user, tmp);
+        else if (strcmp (type, "PHW") == 0)
+            pecan_contact_set_work_phone (user, tmp);
+        else if (strcmp (type, "PHM") == 0)
+            pecan_contact_set_mobile_phone (user, tmp);
+        else if (strcmp (type, "MFN") == 0)
+            purple_connection_set_display_name (gc, tmp);
+        g_free (tmp);
+    }
+    else
+    {
+        if (strcmp (type, "PHH") == 0)
+            pecan_contact_set_home_phone (user, NULL);
+        else if (strcmp (type, "PHW") == 0)
+            pecan_contact_set_work_phone (user, NULL);
+        else if (strcmp (type, "PHM") == 0)
+            pecan_contact_set_mobile_phone (user, NULL);
+    }
 }
 
 static void
-lsg_cmd(MsnCmdProc *cmdproc, MsnCommand *cmd)
+lsg_cmd (MsnCmdProc *cmdproc,
+         MsnCommand *cmd)
 {
-	MsnSession *session = cmdproc->session;
-	char *name;
-	const gchar *group_guid;
+    MsnSession *session = cmdproc->session;
+    char *name;
+    const gchar *group_guid;
 
-	name = pecan_url_decode(cmd->params[0]);
-	group_guid = cmd->params[1];
+    name = pecan_url_decode (cmd->params[0]);
+    group_guid = cmd->params[1];
 
-	pecan_group_new(session->contactlist, name, group_guid);
+    pecan_group_new (session->contactlist, name, group_guid);
 
-	if ((purple_find_group(name)) == NULL)
-	{
-		PurpleGroup *g = purple_group_new(name);
-		purple_blist_add_group(g, NULL);
-	}
+    if (!purple_find_group (name))
+    {
+        PurpleGroup *g = purple_group_new (name);
+        purple_blist_add_group (g, NULL);
+    }
 
-	g_free (name);
+    g_free (name);
 
-	/* Group of ungroupped buddies */
-	if (!group_guid)
-	{
-		if (session->sync->total_users == 0)
-		{
-			cmdproc->cbs_table = session->sync->old_cbs_table;
-
-			msn_session_finish_login(session);
-
-			msn_sync_destroy(session->sync);
-			session->sync = NULL;
-		}
-		return;
-	}
-}
-
-static void
-lst_cmd(MsnCmdProc *cmdproc, MsnCommand *cmd)
-{
-	MsnSession *session = cmdproc->session;
-	const gchar *passport = NULL;
-	PecanContact *user;
-	gchar *friendly = NULL;
-        const gchar *user_guid = NULL;
-	int list_op = -1;
-        gint type;
-        guint i;
-
-        for (i = 0; i < cmd->param_count; i++)
+    /* Group of ungroupped buddies */
+    if (!group_guid)
+    {
+        if (session->sync->total_users == 0)
         {
-            const char *chopped_str;
+            cmdproc->cbs_table = session->sync->old_cbs_table;
 
-            chopped_str = cmd->params[i] + 2;
+            msn_session_finish_login (session);
 
-            /* Check for Name/email. */
-            if (strncmp (cmd->params[i], "N=", 2) == 0)
-                passport = chopped_str;
-            /* Check for Friendlyname. */
-            else if (strncmp (cmd->params[i], "F=", 2) == 0)
-                friendly = pecan_url_decode (chopped_str);
-            /* Check for Contact GUID. */
-            else if (strncmp (cmd->params[i], "C=", 2) == 0)
-                user_guid = chopped_str;
-            else
-                break;
+            msn_sync_destroy (session->sync);
+            session->sync = NULL;
         }
+        return;
+    }
+}
 
-        list_op = g_ascii_strtod (cmd->params[i++], NULL);
-        type = g_ascii_strtod (cmd->params[i++], NULL);
+static void
+lst_cmd (MsnCmdProc *cmdproc,
+         MsnCommand *cmd)
+{
+    MsnSession *session = cmdproc->session;
+    const gchar *passport = NULL;
+    PecanContact *user;
+    gchar *friendly = NULL;
+    const gchar *user_guid = NULL;
+    int list_op = -1;
+    gint type;
+    guint i;
 
-	user = pecan_contact_new (session->contactlist);
-	pecan_contact_set_passport (user, passport);
-	pecan_contact_set_guid (user, user_guid);
+    for (i = 0; i < cmd->param_count; i++)
+    {
+        const char *chopped_str;
 
-	session->sync->last_user = user;
+        chopped_str = cmd->params[i] + 2;
 
-	/* TODO: This can be improved */
+        /* Check for Name/email. */
+        if (strncmp (cmd->params[i], "N=", 2) == 0)
+            passport = chopped_str;
+        /* Check for Friendlyname. */
+        else if (strncmp (cmd->params[i], "F=", 2) == 0)
+            friendly = pecan_url_decode (chopped_str);
+        /* Check for Contact GUID. */
+        else if (strncmp (cmd->params[i], "C=", 2) == 0)
+            user_guid = chopped_str;
+        else
+            break;
+    }
 
-        if (list_op & MSN_LIST_FL_OP)
+    list_op = g_ascii_strtod (cmd->params[i++], NULL);
+    type = g_ascii_strtod (cmd->params[i++], NULL);
+
+    user = pecan_contact_new (session->contactlist);
+    pecan_contact_set_passport (user, passport);
+    pecan_contact_set_guid (user, user_guid);
+
+    session->sync->last_user = user;
+
+    /* TODO: This can be improved */
+
+    if (list_op & MSN_LIST_FL_OP)
+    {
+        if (cmd->params[i])
         {
-            if (cmd->params[i])
+            gchar **c;
+            gchar **tokens;
+            const gchar *group_guids;
+            GSList *group_ids;
+
+            group_guids = cmd->params[i];
+
+            group_ids = NULL;
+
+            tokens = g_strsplit (group_guids, ",", -1);
+
+            for (c = tokens; *c; c++)
             {
-                gchar **c;
-                gchar **tokens;
-                const gchar *group_guids;
-                GSList *group_ids;
-
-                group_guids = cmd->params[i];
-
-                group_ids = NULL;
-
-                tokens = g_strsplit (group_guids, ",", -1);
-
-                for (c = tokens; *c != NULL; c++)
-                {
-                    group_ids = g_slist_append (group_ids, g_strdup (*c));
-                }
-
-                g_strfreev (tokens);
-
-                msn_got_lst_contact (session, user, friendly, list_op, group_ids);
-
-                g_slist_foreach (group_ids, (GFunc)g_free, NULL);
-                g_slist_free (group_ids);
+                group_ids = g_slist_append (group_ids, g_strdup (*c));
             }
-            else
-            {
-                msn_got_lst_contact (session, user, friendly, list_op, NULL);
-            }
+
+            g_strfreev (tokens);
+
+            msn_got_lst_contact (session, user, friendly, list_op, group_ids);
+
+            g_slist_foreach (group_ids, (GFunc) g_free, NULL);
+            g_slist_free (group_ids);
         }
         else
         {
             msn_got_lst_contact (session, user, friendly, list_op, NULL);
         }
+    }
+    else
+    {
+        msn_got_lst_contact (session, user, friendly, list_op, NULL);
+    }
 
-	g_free (friendly);
+    g_free (friendly);
 
-	session->sync->num_users++;
+    session->sync->num_users++;
 
-        if (session->sync->num_users == session->sync->total_users)
-	{
-		cmdproc->cbs_table = session->sync->old_cbs_table;
+    if (session->sync->num_users == session->sync->total_users)
+    {
+        cmdproc->cbs_table = session->sync->old_cbs_table;
 
-		msn_session_finish_login(session);
+        msn_session_finish_login (session);
 
-		msn_sync_destroy(session->sync);
-		session->sync = NULL;
-	}
+        msn_sync_destroy (session->sync);
+        session->sync = NULL;
+    }
 }
 
 static void
-bpr_cmd(MsnCmdProc *cmdproc, MsnCommand *cmd)
+bpr_cmd (MsnCmdProc *cmdproc,
+         MsnCommand *cmd)
 {
-	MsnSync *sync = cmdproc->session->sync;
-	const char *type, *value;
-	PecanContact *user;
+    MsnSync *sync = cmdproc->session->sync;
+    const char *type, *value;
+    PecanContact *user;
 
-	user = sync->last_user;
+    user = sync->last_user;
 
-	g_return_if_fail(user != NULL);
+    g_return_if_fail (user);
 
-	type     = cmd->params[0];
-	value    = cmd->params[1];
+    type = cmd->params[0];
+    value = cmd->params[1];
 
-	if (value)
-	{
-		if (!strcmp(type, "MOB"))
-		{
-			if (!strcmp(value, "Y"))
-				user->mobile = TRUE;
-		}
-		else
-		{
-			gchar *tmp;
-			tmp = pecan_url_decode (value);
-			if (!strcmp(type, "PHH"))
-				pecan_contact_set_home_phone(user, tmp);
-			else if (!strcmp(type, "PHW"))
-				pecan_contact_set_work_phone(user, tmp);
-			else if (!strcmp(type, "PHM"))
-				pecan_contact_set_mobile_phone(user, tmp);
-			g_free (tmp);
-		}
-	}
+    if (value)
+    {
+        if (strcmp(type, "MOB") == 0)
+        {
+            if (strcmp (value, "Y") == 0)
+                user->mobile = TRUE;
+        }
+        else
+        {
+            gchar *tmp;
+            tmp = pecan_url_decode (value);
+            if (strcmp(type, "PHH") == 0)
+                pecan_contact_set_home_phone (user, tmp);
+            else if (strcmp(type, "PHW") == 0)
+                pecan_contact_set_work_phone (user, tmp);
+            else if (strcmp (type, "PHM") == 0)
+                pecan_contact_set_mobile_phone (user, tmp);
+            g_free (tmp);
+        }
+    }
 }
 
 void
-msn_sync_init(void)
+msn_sync_init (void)
 {
-	/* TODO: check prp, blp, bpr */
+    /* TODO: check prp, blp, bpr */
 
-	cbs_table = msn_table_new();
+    cbs_table = msn_table_new ();
 
-	/* Syncing */
-	msn_table_add_cmd(cbs_table, NULL, "GTC", NULL);
-	msn_table_add_cmd(cbs_table, NULL, "BLP", blp_cmd);
-	msn_table_add_cmd(cbs_table, NULL, "PRP", prp_cmd);
-	msn_table_add_cmd(cbs_table, NULL, "LSG", lsg_cmd);
-	msn_table_add_cmd(cbs_table, NULL, "LST", lst_cmd);
-	msn_table_add_cmd(cbs_table, NULL, "BPR", bpr_cmd);
+    /* Syncing */
+    msn_table_add_cmd (cbs_table, NULL, "GTC", NULL);
+    msn_table_add_cmd (cbs_table, NULL, "BLP", blp_cmd);
+    msn_table_add_cmd (cbs_table, NULL, "PRP", prp_cmd);
+    msn_table_add_cmd (cbs_table, NULL, "LSG", lsg_cmd);
+    msn_table_add_cmd (cbs_table, NULL, "LST", lst_cmd);
+    msn_table_add_cmd (cbs_table, NULL, "BPR", bpr_cmd);
 }
 
 void
-msn_sync_end(void)
+msn_sync_end (void)
 {
-	msn_table_destroy(cbs_table);
+    msn_table_destroy (cbs_table);
 }
 
 MsnSync *
-msn_sync_new(MsnSession *session)
+msn_sync_new (MsnSession *session)
 {
-	MsnSync *sync;
+    MsnSync *sync;
 
-	sync = g_new0(MsnSync, 1);
+    sync = g_new0 (MsnSync, 1);
 
-	sync->session = session;
-	sync->cbs_table = cbs_table;
+    sync->session = session;
+    sync->cbs_table = cbs_table;
 
-	return sync;
+    return sync;
 }
 
 void
-msn_sync_destroy(MsnSync *sync)
+msn_sync_destroy (MsnSync *sync)
 {
-	g_free(sync);
+    g_free (sync);
 }
