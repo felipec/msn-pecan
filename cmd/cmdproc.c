3fb328ac92fdaa5d2a587345bd23999bb99a1cbe
Felipe Contreras <felipe.contreras@gmail.com>
2009-03-15 15:05:45 +0200

    cmdproc: trivial cleanup
    No functional changes.
    
    Signed-off-by: Felipe Contreras <felipe.contreras@gmail.com>

--- cmd/cmdproc.c
+++ cmd/cmdproc.c
@@ -38,30 +38,30 @@
 #include "session.h"
 
 MsnCmdProc *
-msn_cmdproc_new()
+msn_cmdproc_new (void)
 {
     MsnCmdProc *cmdproc;
 
-    cmdproc = g_new0(MsnCmdProc, 1);
+    cmdproc = g_new0 (MsnCmdProc, 1);
 
-    cmdproc->history = msn_history_new();
+    cmdproc->history = msn_history_new ();
 
     return cmdproc;
 }
 
 void
-msn_cmdproc_destroy(MsnCmdProc *cmdproc)
+msn_cmdproc_destroy (MsnCmdProc *cmdproc)
 {
     pecan_log ("begin");
 
     pecan_debug ("cmdproc=%p", cmdproc);
 
-    msn_history_destroy(cmdproc->history);
+    msn_history_destroy (cmdproc->history);
 
-    if (cmdproc->last_cmd != NULL)
-        msn_command_destroy(cmdproc->last_cmd);
+    if (cmdproc->last_cmd)
+        msn_command_destroy (cmdproc->last_cmd);
 
-    g_free(cmdproc);
+    g_free (cmdproc);
 
     pecan_log ("end");
 }
@@ -79,52 +79,53 @@ msn_cmdproc_flush (MsnCmdProc *cmdproc)
 }
 
 static void
-show_debug_cmd(MsnCmdProc *cmdproc, gboolean incoming, const char *command)
+show_debug_cmd (MsnCmdProc *cmdproc,
+                gboolean incoming,
+                const char *command)
 {
     char *show;
     char tmp;
     size_t len;
 
-    len = strlen(command);
-    show = g_strdup(command);
+    len = strlen (command);
+    show = g_strdup (command);
 
     tmp = (incoming) ? 'S' : 'C';
 
     if ((show[len - 1] == '\n') && (show[len - 2] == '\r'))
-    {
         show[len - 2] = '\0';
-    }
 
     pecan_info ("%c: %03d: %s", tmp, cmdproc->cmd_count, show);
 
-    g_free(show);
+    g_free (show);
 }
 
 void
-msn_cmdproc_send_trans(MsnCmdProc *cmdproc, MsnTransaction *trans)
+msn_cmdproc_send_trans (MsnCmdProc *cmdproc,
+                        MsnTransaction *trans)
 {
     char *data;
     size_t len;
 
-    g_return_if_fail(cmdproc != NULL);
-    g_return_if_fail(trans != NULL);
+    g_return_if_fail (cmdproc);
+    g_return_if_fail (trans);
 
-    msn_history_add(cmdproc->history, trans);
+    msn_history_add (cmdproc->history, trans);
 
-    data = msn_transaction_to_string(trans);
+    data = msn_transaction_to_string (trans);
 
-    len = strlen(data);
+    len = strlen (data);
 
-    show_debug_cmd(cmdproc, FALSE, data);
+    show_debug_cmd (cmdproc, FALSE, data);
 
-    if (trans->callbacks == NULL)
-        trans->callbacks = g_hash_table_lookup(cmdproc->cbs_table->cmds,
-                                               trans->command);
+    if (!trans->callbacks)
+        trans->callbacks = g_hash_table_lookup (cmdproc->cbs_table->cmds,
+                                                trans->command);
 
-    if (trans->payload != NULL)
+    if (trans->payload)
     {
-        data = g_realloc(data, len + trans->payload_len);
-        memcpy(data + len, trans->payload, trans->payload_len);
+        data = g_realloc (data, len + trans->payload_len);
+        memcpy (data + len, trans->payload, trans->payload_len);
         len += trans->payload_len;
     }
 
@@ -137,21 +138,23 @@ msn_cmdproc_send_trans(MsnCmdProc *cmdproc, MsnTransaction *trans)
             pecan_node_error (cmdproc->conn);
     }
 
-    g_free(data);
+    g_free (data);
 }
 
 void
-msn_cmdproc_send_quick(MsnCmdProc *cmdproc, const char *command,
-                       const char *format, ...)
+msn_cmdproc_send_quick (MsnCmdProc *cmdproc,
+                        const char *command,
+                        const char *format,
+                        ...)
 {
     char *data;
     char *params = NULL;
     size_t len;
 
-    g_return_if_fail(cmdproc != NULL);
-    g_return_if_fail(command != NULL);
+    g_return_if_fail (cmdproc);
+    g_return_if_fail (command);
 
-    if (format != NULL)
+    if (format)
     {
         va_list args;
         va_start (args, format);
@@ -159,16 +162,16 @@ msn_cmdproc_send_quick(MsnCmdProc *cmdproc, const char *command,
         va_end (args);
     }
 
-    if (params != NULL)
+    if (params)
         data = g_strdup_printf ("%s %s\r\n", command, params);
     else
         data = g_strdup_printf ("%s\r\n", command);
 
-    g_free(params);
+    g_free (params);
 
-    len = strlen(data);
+    len = strlen (data);
 
-    show_debug_cmd(cmdproc, FALSE, data);
+    show_debug_cmd (cmdproc, FALSE, data);
 
     {
         GIOStatus status;
@@ -179,7 +182,7 @@ msn_cmdproc_send_quick(MsnCmdProc *cmdproc, const char *command,
             pecan_node_error (cmdproc->conn);
     }
 
-    g_free(data);
+    g_free (data);
 }
 
 void
@@ -190,17 +193,15 @@ msn_cmdproc_send_valist (MsnCmdProc *cmdproc,
 {
     MsnTransaction *trans;
 
-    g_return_if_fail (cmdproc != NULL);
-    g_return_if_fail (command != NULL);
+    g_return_if_fail (cmdproc);
+    g_return_if_fail (command);
 
     trans = g_new0 (MsnTransaction, 1);
 
     trans->command = g_strdup (command);
 
-    if (format != NULL)
-    {
+    if (format)
         trans->params = g_strdup_vprintf (format, args);
-    }
 
     msn_cmdproc_send_trans (cmdproc, trans);
 }
@@ -211,8 +212,8 @@ msn_cmdproc_send (MsnCmdProc *cmdproc,
                   const char *format,
                   ...)
 {
-    g_return_if_fail (cmdproc != NULL);
-    g_return_if_fail (command != NULL);
+    g_return_if_fail (cmdproc);
+    g_return_if_fail (command);
 
     {
         va_list args;
@@ -223,48 +224,51 @@ msn_cmdproc_send (MsnCmdProc *cmdproc,
 }
 
 void
-msn_cmdproc_process_payload(MsnCmdProc *cmdproc, char *payload,
-                            int payload_len)
+msn_cmdproc_process_payload (MsnCmdProc *cmdproc,
+                             char *payload,
+                             int payload_len)
 {
     MsnCommand *last;
 
-    g_return_if_fail(cmdproc != NULL);
+    g_return_if_fail (cmdproc);
 
     last = cmdproc->last_cmd;
-    last->payload = g_memdup(payload, payload_len);
+    last->payload = g_memdup (payload, payload_len);
     last->payload_len = payload_len;
 
-    if (last->payload_cb != NULL)
-        last->payload_cb(cmdproc, last, payload, payload_len);
+    if (last->payload_cb)
+        last->payload_cb (cmdproc, last, payload, payload_len);
 }
 
 void
-msn_cmdproc_process_msg(MsnCmdProc *cmdproc, MsnMessage *msg)
+msn_cmdproc_process_msg (MsnCmdProc *cmdproc,
+                         MsnMessage *msg)
 {
     MsnMsgTypeCb cb;
 
-    if (msn_message_get_content_type(msg) == NULL)
+    if (!msn_message_get_content_type (msg))
     {
         pecan_warning ("failed to find message content");
         return;
     }
 
-    cb = g_hash_table_lookup(cmdproc->cbs_table->msgs,
-                             msn_message_get_content_type(msg));
+    cb = g_hash_table_lookup (cmdproc->cbs_table->msgs,
+                              msn_message_get_content_type (msg));
 
-    if (cb == NULL)
+    if (!cb)
     {
         pecan_warning ("unhandled content-type: [%s]",
-                     msn_message_get_content_type (msg));
+                       msn_message_get_content_type (msg));
 
         return;
     }
 
-    cb(cmdproc, msg);
+    cb (cmdproc, msg);
 }
 
 void
-msn_cmdproc_process_cmd(MsnCmdProc *cmdproc, MsnCommand *cmd)
+msn_cmdproc_process_cmd (MsnCmdProc *cmdproc,
+                         MsnCommand *cmd)
 {
     MsnTransCb cb = NULL;
     MsnTransaction *trans = NULL;
@@ -274,80 +278,67 @@ msn_cmdproc_process_cmd(MsnCmdProc *cmdproc, MsnCommand *cmd)
     pecan_log ("begin");
 
     if (cmd->trId)
-    {
-        cmd->trans = trans = msn_history_find(cmdproc->history, cmd->trId);
-    }
+        cmd->trans = trans = msn_history_find (cmdproc->history, cmd->trId);
 
     /* transaction finished. clear timeouts. */
     if (trans)
         msn_transaction_flush (trans);
 
-    if (g_ascii_isdigit(cmd->command[0]))
+    if (g_ascii_isdigit (cmd->command[0]))
     {
-        if (trans != NULL)
+        if (trans)
         {
             MsnErrorCb error_cb = NULL;
             int error;
 
-            error = atoi(cmd->command);
+            error = atoi (cmd->command);
 
-            if (trans->error_cb != NULL)
+            if (trans->error_cb)
                 error_cb = trans->error_cb;
 
-            if (error_cb == NULL && cmdproc->cbs_table->errors != NULL)
-                error_cb = g_hash_table_lookup(cmdproc->cbs_table->errors, trans->command);
+            if (!error_cb && cmdproc->cbs_table->errors)
+                error_cb = g_hash_table_lookup (cmdproc->cbs_table->errors, trans->command);
 
             if (!error_cb)
                 error_cb = cmdproc->error_handler;
 
-            if (error_cb != NULL)
-            {
-                error_cb(cmdproc, trans, error);
-            }
+            if (error_cb)
+                error_cb (cmdproc, trans, error);
             else
-            {
-                pecan_error ("unhandled error: [%s]",
-                           cmd->command);
-            }
+                pecan_error ("unhandled error: [%s]", cmd->command);
 
             return;
         }
     }
 
-    if (cmdproc->cbs_table->async != NULL)
-        cb = g_hash_table_lookup(cmdproc->cbs_table->async, cmd->command);
+    if (cmdproc->cbs_table->async)
+        cb = g_hash_table_lookup (cmdproc->cbs_table->async, cmd->command);
 
-    if (cb == NULL && trans != NULL)
-    {
-        if (trans->callbacks != NULL)
-            cb = g_hash_table_lookup(trans->callbacks, cmd->command);
-    }
+    if (!cb)
+        if (trans && trans->callbacks)
+            cb = g_hash_table_lookup (trans->callbacks, cmd->command);
 
-    if (cb == NULL && cmdproc->cbs_table->fallback != NULL)
-        cb = g_hash_table_lookup(cmdproc->cbs_table->fallback, cmd->command);
+    if (!cb && cmdproc->cbs_table->fallback)
+        cb = g_hash_table_lookup (cmdproc->cbs_table->fallback, cmd->command);
 
-    if (cb != NULL)
-    {
-        cb(cmdproc, cmd);
-    }
+    if (cb)
+        cb (cmdproc, cmd);
     else
-    {
-        pecan_warning ("unhandled command: [%s]",
-                     cmd->command);
-    }
+        pecan_warning ("unhandled command: [%s]", cmd->command);
 
     pecan_log ("end");
 }
 
 void
-msn_cmdproc_process_cmd_text(MsnCmdProc *cmdproc, const char *command)
+msn_cmdproc_process_cmd_text (MsnCmdProc *cmdproc,
+                              const char *command)
 {
-    show_debug_cmd(cmdproc, TRUE, command);
+    show_debug_cmd (cmdproc, TRUE, command);
 
-    if (cmdproc->last_cmd != NULL)
-        msn_command_destroy(cmdproc->last_cmd);
+    if (cmdproc->last_cmd)
+        msn_command_destroy (cmdproc->last_cmd);
 
-    cmdproc->last_cmd = msn_command_from_string(command);
+    cmdproc->last_cmd = msn_command_from_string (command);
 
-    msn_cmdproc_process_cmd(cmdproc, cmdproc->last_cmd);
+    msn_cmdproc_process_cmd (cmdproc, cmdproc->last_cmd);
 }
