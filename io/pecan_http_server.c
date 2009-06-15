fd2ba8cab2e3479a50db849eb589ec4f9bbb90d2
Felipe Contreras <felipe.contreras@gmail.com>
2009-06-15 23:04:59 +0300

    Optimize GType retrieval
    Signed-off-by: Felipe Contreras <felipe.contreras@gmail.com>

--- io/pecan_http_server.c
+++ io/pecan_http_server.c
@@ -932,7 +932,7 @@ pecan_http_server_get_type (void)
 {
     static GType type = 0;
 
-    if (type == 0) 
+    if (G_UNLIKELY (type == 0))
     {
         GTypeInfo *type_info;
 
--- io/pecan_node.c
+++ io/pecan_node.c
@@ -685,7 +685,7 @@ pecan_node_get_type (void)
 {
     static GType type = 0;
 
-    if (type == 0)
+    if (G_UNLIKELY (type == 0))
     {
         GTypeInfo *type_info;
 
