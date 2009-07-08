505298d64ff309adffae0a4cabe8281467359cb4
Felipe Contreras <felipe.contreras@gmail.com>
2009-07-08 03:21:59 +0300

    Make printf test work again
    Signed-off-by: Felipe Contreras <felipe.contreras@gmail.com>

--- tests/printf.c
+++ tests/printf.c
@@ -16,48 +16,74 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  */
 
-#include <glib.h>
-#include "pecan_printf.h"
+#include <check.h>
+#include "pn_printf.h"
 
-void
-pecan_printf (const gchar *expect,
-              const gchar *format,
-              ...)
+static inline void
+t_printf (const char *expect,
+          const char *format,
+          ...)
 {
-    gchar *buffer;
+    char *buffer;
     va_list args;
 
     va_start (args, format);
-    buffer = pecan_strdup_vprintf (format, args);
+    buffer = pn_strdup_vprintf (format, args);
     va_end (args);
 
-    g_printf ("[%s] %s\n", buffer, (strcmp (expect, buffer) == 0) ? "OK" : "fail" );
+    ck_assert_str_eq (buffer, expect);
 
     g_free (buffer);
 }
 
-int
-main (int argc,
-      char *argv[])
+START_TEST (test_printf)
 {
-    g_type_init ();
+    t_printf ("hello world!", "hello world!");
+    t_printf ("hello world!", "hello %s!", "world");
+    t_printf ("(null)", "%s", NULL);
+    t_printf ("3", "%d", 3);
+    t_printf ("3", "%i", 3);
+    t_printf ("-3", "%d", -3);
+    t_printf ("0xd", "%p", 13);
+    t_printf ("(nil)", "%p", NULL);
+    t_printf ("3", "%u", 3);
+    t_printf ("4294967293", "%u", -3);
+    t_printf ("3", "%lu", 3);
+    t_printf ("4294967293", "%lu", -3);
+    t_printf ("a", "%c", 'a');
+    t_printf ("D", "%X", 13);
+    t_printf ("   D", "%4X", 13);
+    t_printf ("000D", "%.4X", 13);
+    t_printf ("000D", "%04X", 13);
 
-    pecan_printf ("hello world!", "hello world!");
-    pecan_printf ("hello world!", "hello %s!", "world");
-    pecan_printf ("3", "%d", 3);
-    pecan_printf ("3", "%i", 3);
-    pecan_printf ("-3", "%d", -3);
-    pecan_printf ("0xd", "%p", 13);
-    pecan_printf ("(nil)", "%p", NULL);
-    pecan_printf ("3", "%u", 3);
-    pecan_printf ("4294967293", "%u", -3);
-    pecan_printf ("3", "%lu", 3);
-    pecan_printf ("4294967293", "%lu", -3);
-    pecan_printf ("a", "%c", 'a');
-    pecan_printf ("D", "%X", 13);
-    pecan_printf ("   D", "%4X", 13);
-    pecan_printf ("000D", "%.4X", 13);
-    pecan_printf ("000D", "%04X", 13);
+}
+END_TEST
 
-    return 0;
+Suite *
+util_suite (void)
+{
+    Suite *s = suite_create ("util");
+
+    /* Core test case */
+    TCase *tc_core = tcase_create ("Core");
+    tcase_add_test (tc_core, test_printf);
+    suite_add_tcase (s, tc_core);
+
+    return s;
+}
+
+int
+main (void)
+{
+    int number_failed;
+    Suite *s;
+    SRunner *sr;
+
+    s = util_suite ();
+    sr = srunner_create (s);
+    srunner_run_all (sr, CK_NORMAL);
+    number_failed = srunner_ntests_failed (sr);
+    srunner_free (sr);
+
+    return (number_failed == 0) ? 0 : 1;
 }
