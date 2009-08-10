d9cd959b3747de604e8d565a9f70cadcfbe3151d
Devid Antonio Filoni <devidfil@gmail.com>
2009-08-10 15:39:35 +0200

    Fix build with libpurple < 2.5.0
    Signed-off-by: Devid Antonio Filoni <devidfil@gmail.com>
    Signed-off-by: Felipe Contreras <felipe.contreras@gmail.com>

--- switchboard.c
+++ switchboard.c
@@ -1414,6 +1414,7 @@ datacast_msg (MsnCmdProc *cmdproc,
 
     if (strcmp (id, "1") == 0)
     {
+#if PURPLE_VERSION_CHECK(2,5,0)
         MsnSwitchBoard *swboard;
 
         swboard = cmdproc->data;
@@ -1424,6 +1425,9 @@ datacast_msg (MsnCmdProc *cmdproc,
             purple_prpl_got_attention_in_chat (connection, swboard->chat_id, passport, 0);
         else
             purple_prpl_got_attention (connection, passport, 0);
+#else
+        serv_got_attention (account->gc, passport, 0);
+#endif /* PURPLE_VERSION_CHECK(2,5,0) */
     }
     else if (strcmp (id, "2") == 0)
     {
