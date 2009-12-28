06f5957693dc525e1278f977a105c85b66005c18
Felipe Contreras <felipe.contreras@gmail.com>
2009-12-28 20:41:41 +0200

    io/dc: add read_impl
    Signed-off-by: Felipe Contreras <felipe.contreras@gmail.com>

--- io/pn_dc_conn.c
+++ io/pn_dc_conn.c
@@ -20,8 +20,16 @@
 #include "pn_node_private.h"
 #include "pn_log.h"
 
+#include <string.h> /* for memcpy */
+#include <stdint.h>
+
 struct PnDcConn {
     PnNode parent;
+
+    gchar *rx_buf;
+    gsize rx_len;
+    gboolean need_more;
+    guint32 body_len;
 };
 
 struct PnDcConnClass {
@@ -91,12 +99,97 @@ write_impl(PnNode *conn,
     return status;
 }
 
+static GIOStatus
+read_impl(PnNode *conn,
+          gchar *buf,
+          gsize count,
+          gsize *ret_bytes_read,
+          GError **error)
+{
+    GIOStatus status = G_IO_STATUS_NORMAL;
+    PnDcConn *dc_conn = PN_DC_CONN(conn);
+    gsize length;
+
+    pn_debug("name=%s", conn->name);
+
+    if (dc_conn->need_more) {
+        gsize bytes_read;
+
+        status = pn_stream_read(conn->stream, buf, count, &bytes_read, NULL);
+
+        if (status != G_IO_STATUS_NORMAL && status != G_IO_STATUS_AGAIN)
+            goto leave;
+
+        /* append buf to rx_buf */
+        dc_conn->rx_buf = g_realloc(dc_conn->rx_buf, bytes_read + dc_conn->rx_len + 1);
+        memcpy(dc_conn->rx_buf + dc_conn->rx_len, buf, bytes_read + 1);
+        dc_conn->rx_len += bytes_read;
+
+        if (status == G_IO_STATUS_AGAIN)
+            goto leave;
+    }
+
+    if (!dc_conn->body_len) {
+        if (dc_conn->rx_len < 4) {
+            dc_conn->need_more = TRUE;
+            status = G_IO_STATUS_AGAIN;
+            goto leave;
+        }
+
+        dc_conn->body_len = GUINT32_FROM_LE(*(uint32_t *) dc_conn->rx_buf);
+    }
+
+    length = dc_conn->body_len + 4;
+
+    if (dc_conn->rx_len < length) {
+        dc_conn->need_more = TRUE;
+        status = G_IO_STATUS_AGAIN;
+        goto leave;
+    }
+
+    memcpy(buf, dc_conn->rx_buf + 4, dc_conn->body_len);
+    if (ret_bytes_read)
+        *ret_bytes_read = dc_conn->body_len;
+    dc_conn->body_len = 0;
+
+    {
+        gchar *tmp;
+
+        dc_conn->rx_len -= length;
+
+        tmp = dc_conn->rx_buf;
+
+        if (dc_conn->rx_len > 0) {
+            dc_conn->rx_buf = g_memdup(dc_conn->rx_buf + length, dc_conn->rx_len);
+            dc_conn->need_more = FALSE;
+        } else {
+            dc_conn->rx_buf = NULL;
+            dc_conn->need_more = TRUE;
+        }
+
+        g_free(tmp);
+    }
+
+leave:
+    return status;
+}
+
 /* GObject stuff. */
 
 static void
 finalize(GObject *obj)
 {
+    PnDcConn *dc_conn = PN_DC_CONN(obj);
+    g_free(dc_conn->rx_buf);
     G_OBJECT_CLASS(parent_class)->finalize(obj);
+}
+
+static void
+instance_init(GTypeInstance *instance,
+              gpointer g_class)
+{
+    PnDcConn *dc_conn = PN_DC_CONN(instance);
+    dc_conn->need_more = TRUE;
 }
 
 static void
@@ -107,6 +200,7 @@ class_init(gpointer g_class,
     GObjectClass *gobject_class = G_OBJECT_CLASS(g_class);
 
     conn_class->write = &write_impl;
+    conn_class->read = &read_impl;
 
     gobject_class->finalize = finalize;
 
@@ -124,6 +218,7 @@ pn_dc_conn_get_type(void)
             .class_size = sizeof(PnDcConnClass),
             .class_init = class_init,
             .instance_size = sizeof(PnDcConn),
+            .instance_init = instance_init,
         };
 
         tmp_type = g_type_register_static(PN_NODE_TYPE, "PnDcConnType", &type_info, 0);
