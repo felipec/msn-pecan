b85735161bca267048a4353971947286b3ab37ce
Felipe Contreras <felipe.contreras@gmail.com>
2010-01-25 05:29:53 +0200

    directconn: avoid extra steps in send_msg()
    Signed-off-by: Felipe Contreras <felipe.contreras@gmail.com>

--- cvr/pn_direct_conn.c
+++ cvr/pn_direct_conn.c
@@ -146,6 +146,11 @@ pn_direct_conn_send_msg(struct pn_direct_conn *direct_conn, MsnMessage *msg)
 
     body = msn_message_gen_slp_body(msg, &body_len);
 
+    if (direct_conn->status == 1) {
+        async_write(direct_conn, NULL, NULL, body, body_len, NULL, NULL);
+        return;
+    }
+
     direct_conn->last_msg = msn_message_ref(msg);
     async_write(direct_conn, msg_cb, msg, body, body_len, NULL, NULL);
 }
